0f8b316a4978814df4ed7f84a436989b0d851e6d
sourav-18 <souravhd2@gmail.com>
2025-10-05 23:43:38 +0530

    render setup

--- backend/db/connection.db.js
+++ backend/db/connection.db.js
@@ -4,12 +4,28 @@ const serverEnv = require("../utils/serverEnv.utils");
 const sequelize = new Sequelize(serverEnv.DB_NAME, serverEnv.DB_USER, serverEnv.DB_PASSWORD, {
     host: serverEnv.DB_HOST,
     dialect: 'postgres',
-    logging:false
-
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
 });
+
+// const sequelize = new Sequelize('', {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false 
+//         }
+//     },
+//     logging: false
+// });
 
 sequelize.authenticate().then(() => console.log("DB connect successfully")).catch((err) => console.log("DB Connection Error:", err));
 
-sequelize.sync({alter:serverEnv.SERVER_ENV==="dev"?true:false}).then(() => console.log("All DB synced")).catch((err) => console.log("DB Sync Error:", err));
+sequelize.sync({ alter: serverEnv.SERVER_ENV === "dev" ? true : false }).then(() => console.log("All DB synced")).catch((err) => console.log("DB Sync Error:", err));
 
 module.exports = sequelize;
